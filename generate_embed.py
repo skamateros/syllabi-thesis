46e137c9d73601efdbc7c4019f23b8f4b5b45a25
skamateros <181945193+skamateros@users.noreply.github.com>
2026-02-20 17:18:05 +0100

    experimenting with embedding data structures

---
+++ generate_embed.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+import json
+
+from sentence_transformers import SentenceTransformer
+
+def main():
+    with open('data/SU.heuristics.json', 'r') as f:
+        corpus = json.load(f)
+    
+    model = SentenceTransformer('KBLab/sentence-bert-swedish-cased')
+
+    # print(corpus['Course-list'][0])
+
+    corpus_embeds = {}
+
+    for course in corpus['Course-list']:
+        corpus_embeds[course['CourseCode']] = { 'contents': model.encode(course['CourseContent']) , 'outcomes': [model.encode(outcome) for outcome in course['ILO-list-sv']] }
+
+    class NumpyEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, np.ndarray):
+                return obj.tolist()
+            if isinstance(obj, (np.integer, np.floating)):
+                return obj.item()
+            return super().default(obj)
+
+    with open('data/SU.embeddings.json', 'w') as f:
+        json.dump(corpus_embeds, f, cls=NumpyEncoder)
+
+if __name__ == '__main__':
+    main()
