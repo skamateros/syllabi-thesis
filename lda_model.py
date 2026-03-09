27fdbedd7dd2b36431c098e2f7ad182e1c60bdba
skamateros <181945193+skamateros@users.noreply.github.com>
2026-03-09 16:17:21 +0100

    Fixed LDA model tokenization and added TF-IDF implementation

--- lda_model.py
+++ lda_model.py
@@ -15,15 +15,23 @@ def main():
     with open('data/SU.heuristics.json', 'r') as f:
         corpus = json.load(f)
 
-    def tokenize(text):
-        doc = nlp(text)
-        tokens = [token.lemma.lower() for sentence in doc.sentences for token in sentence.words if token.lemma.lower() not in stop_words and token.pos != 'PUNCT']
-        return tokens
+    print('Tokenizing and lemmatizing corpus...')
 
-    print('Iterating over corpus and tokenizing...')
-    for i, course in enumerate(corpus['Course-list']):
-        course['CourseContent'] = tokenize(course['CourseContent'])
-        course['ILO-list-sv'] = [tokenize(outcome) for outcome in course['ILO-list-sv']]
+    all_texts = []
+    for course in corpus['Course-list']:
+        # course['CourseContent'] = tokenize(course['CourseContent'])
+        # course['ILO-list-sv'] = [tokenize(outcome) for outcome in course['ILO-list-sv']]
+        all_texts.append(course['CourseContent'])
+        all_texts.extend(course['ILO-list-sv'])
+    
+    docs = nlp.bulk_process(all_texts)
+
+    idx = 0
+    for course in corpus['Course-list']:
+        course['CourseContent'] = [token.lemma.lower() for sent in docs[idx].sentences for token in sent.words if token.pos != 'PUNCT' and token.lemma.lower() not in stop_words]
+        idx += 1
+        course['ILO-list-sv'] = [[token.lemma.lower() for token in docs[idx + j].sentences[0].words if token.pos != 'PUNCT' and token.lemma.lower() not in stop_words] for j in range(len(course['ILO-list-sv']))]
+        idx += len(course['ILO-list-sv'])
 
     texts = [course['CourseContent'] for course in corpus['Course-list']] + [outcome for course in corpus['Course-list'] for outcome in course['ILO-list-sv']]
 
---
+++ tfidf.py
@@ -0,0 +1,61 @@
+import json
+import stanza
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import torch
+
+def main():
+    device = 'mps' if torch.backends.mps.is_available() else 'cpu'
+
+    nlp = stanza.Pipeline(lang='sv', processors = 'tokenize,pos,lemma', device=device)
+    nltk.download('stopwords')
+    stop_words = set(nltk.corpus.stopwords.words('swedish'))
+
+    with open('data/SU.heuristics.json', 'r') as f:
+        corpus = json.load(f)
+
+    print('Tokenizing and lemmatizing corpus...')
+
+    all_texts = []
+    for course in corpus['Course-list']:
+        all_texts.append(course['CourseContent'])
+        all_texts.extend(course['ILO-list-sv'])
+    
+    docs = nlp.bulk_process(all_texts)
+
+    idx = 0
+    for course in corpus['Course-list']:
+        course['CourseContent'] = [token.lemma.lower() for sent in docs[idx].sentences for token in sent.words if token.pos != 'PUNCT' and token.lemma.lower() not in stop_words]
+        idx += 1
+        course['ILO-list-sv'] = [[token.lemma.lower() for token in docs[idx + j].sentences[0].words if token.pos != 'PUNCT' and token.lemma.lower() not in stop_words] for j in range(len(course['ILO-list-sv']))]
+        idx += len(course['ILO-list-sv'])
+
+    def tokens2string(tokens):
+        return ' '.join(tokens)
+
+    all_texts = [tokens2string(course['CourseContent']) for course in corpus['Course-list']] + [tokens2string(outcome) for course in corpus['Course-list'] for outcome in course['ILO-list-sv']]
+
+    print('Fitting TF-IDF vectorizer...')
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit(all_texts)
+
+    print('Calculating similarities...')
+    output = {}
+    for course in corpus['Course-list']:
+        course_vec = vectorizer.transform([tokens2string(course['CourseContent'])])
+
+        similarities = []
+        for outcome in course['ILO-list-sv']:
+            outcome_vec = vectorizer.transform([tokens2string(outcome)])
+            similarity = cosine_similarity(course_vec, outcome_vec)[0][0]
+            similarities.append(similarity)
+        
+        output[course['CourseCode']] = similarities
+
+    print('Saving similarities to JSON file...')
+    with open('data/SU.tfidf.similarities.json', 'w') as f:
+        json.dump(output, f, indent=2)
+
+if __name__ == '__main__':
+    main()
