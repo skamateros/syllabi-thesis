a43236c2b40caf5e1ca3154ddeb266158a6073f3
skamateros <181945193+skamateros@users.noreply.github.com>
2026-03-10 15:02:25 +0100

    Separated lemmatization step and saved lemmatized corpus

---
+++ lemmatize.py
@@ -0,0 +1,35 @@
+import json
+import stanza
+import nltk
+from torch.backends import mps
+
+def main():
+    device = 'mps' if mps.is_available() else 'cpu'
+    print(f'Using device: {device}')
+
+    nlp = stanza.Pipeline(lang='sv', processors = 'tokenize,pos,lemma', device=device)
+    nltk.download('stopwords')
+    stop_words = set(nltk.corpus.stopwords.words('swedish'))
+
+    with open('data/SU.heuristics.json', 'r') as f:
+        corpus = json.load(f)
+
+    all_texts = []
+    for course in corpus['Course-list']:
+        all_texts.append(course['CourseContent'])
+        all_texts.extend(course['ILO-list-sv'])
+    
+    docs = nlp.bulk_process(all_texts)
+
+    idx = 0
+    for course in corpus['Course-list']:
+        course['CourseContent'] = [token.lemma.lower() for sent in docs[idx].sentences for token in sent.words if token.pos != 'PUNCT' and token.lemma.lower() not in stop_words]
+        idx += 1
+        course['ILO-list-sv'] = [[token.lemma.lower() for token in docs[idx + j].sentences[0].words if token.pos != 'PUNCT' and token.lemma.lower() not in stop_words] for j in range(len(course['ILO-list-sv']))]
+        idx += len(course['ILO-list-sv'])
+
+    with open('data/SU.lemmatized.json', 'w') as f:
+        json.dump(corpus, f, indent=2)
+
+if __name__ == '__main__':
+    main()
--- tfidf.py
+++ tfidf.py
@@ -1,35 +1,40 @@
 import json
-import stanza
-import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import torch
 
 def main():
-    device = 'mps' if torch.backends.mps.is_available() else 'cpu'
+    # device = 'mps' if torch.backends.mps.is_available() else 'cpu'
 
-    nlp = stanza.Pipeline(lang='sv', processors = 'tokenize,pos,lemma', device=device)
-    nltk.download('stopwords')
-    stop_words = set(nltk.corpus.stopwords.words('swedish'))
+    # nlp = stanza.Pipeline(lang='sv', processors = 'tokenize,pos,lemma', device=device)
+    # nltk.download('stopwords')
+    # stop_words = set(nltk.corpus.stopwords.words('swedish'))
 
-    with open('data/SU.heuristics.json', 'r') as f:
+    # with open('data/SU.heuristics.json', 'r') as f:
+    #     corpus = json.load(f)
+
+    # print('Tokenizing and lemmatizing corpus...')
+
+    # all_texts = []
+    # for course in corpus['Course-list']:
+    #     all_texts.append(course['CourseContent'])
+    #     all_texts.extend(course['ILO-list-sv'])
+    
+    # docs = nlp.bulk_process(all_texts)
+
+    # idx = 0
+    # for course in corpus['Course-list']:
+    #     course['CourseContent'] = [token.lemma.lower() for sent in docs[idx].sentences for token in sent.words if token.pos != 'PUNCT' and token.lemma.lower() not in stop_words]
+    #     idx += 1
+    #     course['ILO-list-sv'] = [[token.lemma.lower() for token in docs[idx + j].sentences[0].words if token.pos != 'PUNCT' and token.lemma.lower() not in stop_words] for j in range(len(course['ILO-list-sv']))]
+    #     idx += len(course['ILO-list-sv'])
+
+    with open('data/SU.lemmatized.json', 'r') as f:
         corpus = json.load(f)
-
-    print('Tokenizing and lemmatizing corpus...')
 
     all_texts = []
     for course in corpus['Course-list']:
         all_texts.append(course['CourseContent'])
         all_texts.extend(course['ILO-list-sv'])
-    
-    docs = nlp.bulk_process(all_texts)
-
-    idx = 0
-    for course in corpus['Course-list']:
-        course['CourseContent'] = [token.lemma.lower() for sent in docs[idx].sentences for token in sent.words if token.pos != 'PUNCT' and token.lemma.lower() not in stop_words]
-        idx += 1
-        course['ILO-list-sv'] = [[token.lemma.lower() for token in docs[idx + j].sentences[0].words if token.pos != 'PUNCT' and token.lemma.lower() not in stop_words] for j in range(len(course['ILO-list-sv']))]
-        idx += len(course['ILO-list-sv'])
 
     def tokens2string(tokens):
         return ' '.join(tokens)
