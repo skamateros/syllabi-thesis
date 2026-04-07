b4de6c3bb288f2a976305742f55a391f40a21bbe
skamateros <181945193+skamateros@users.noreply.github.com>
2026-04-07 18:15:55 +0200

    Changed outlier calculation logic and generated outlier visualisations for each method and direction

--- outliers.py
+++ outliers.py
@@ -1,32 +1,51 @@
 import json
+import numpy as np
+
+def get_threshold(sims):
+    sims_array = np.array(sims)
+    mean = np.mean(sims_array)
+    std_dev = np.std(sims_array)
+    threshold = mean - 2 * std_dev
+    return threshold
 
 for i in ['outcome', 'content']:
+    for j in ['tfidf', 'sbert']:
+        with open(f'data/{i}.{j}.similarities.json', 'r') as f:
+            data = json.load(f)
 
-    with open(f'data/{i}.sbert.similarities.json', 'r') as f:
-        sbert_sims = json.load(f)
-
-    output = []
-    for key, course in sbert_sims.items():
-        if i == 'content':
-            for chunk in course['chunks']:
-                if chunk['max_similarity'] < 0.35:
-                    output.append({
-                        "course_code": key,
-                        "chunk": chunk['chunk'],
-                        "similarity": chunk['max_similarity'],
-                        "matched_outcome": chunk['matched_outcome']
-                    })
-        elif i == 'outcome':
-            for outcome in course['outcomes']:
-                if outcome['max_similarity'] < 0.35:
-                    output.append({
-                        "course_code": key,
-                        "outcome": outcome['outcome'],
-                        "similarity": outcome['max_similarity'],
-                        "matched_chunk": outcome['matched_chunk']
-                    })
-        else:
-            raise ValueError("Invalid i value. Use 'content' or 'outcome'.")
+        threshold = get_threshold([
+            chunk['max_similarity'] 
+            for course in data.values() 
+            for chunk in (course['chunks'] if i == 'content' else course['outcomes'])
+            ])
         
-    with open(f'data/outliers.{i}.json', 'w') as f:
-        json.dump(output, f, indent=2)
+        if threshold < 0:
+            threshold = 0.0
+
+        print(f"Threshold for {i} with {j}: {threshold}")
+        output = []
+        for key, course in data.items():
+            
+            if i == 'content':
+                for chunk in course['chunks']:
+                    if chunk['max_similarity'] <= threshold:
+                        output.append({
+                            "course_code": key,
+                            "chunk": chunk['chunk'],
+                            "similarity": chunk['max_similarity'],
+                            "matched_outcome": chunk['matched_outcome']
+                        })
+            elif i == 'outcome':
+                for outcome in course['outcomes']:
+                    if outcome['max_similarity'] <= threshold:
+                        output.append({
+                            "course_code": key,
+                            "outcome": outcome['outcome'],
+                            "similarity": outcome['max_similarity'],
+                            "matched_chunk": outcome['matched_chunk']
+                        })
+            else:
+                raise ValueError("Invalid i value. Use 'content' or 'outcome'.")
+            
+        with open(f'data/outliers.{i}.{j}.json', 'w') as f:
+            json.dump(output, f, indent=2)
